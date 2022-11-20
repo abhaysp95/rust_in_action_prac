ae19ecbdc503770c0ba78b95545d1bd980e16e60
coolabhays <abhaysp9955@gmail.com>
2022-11-20 17:52:58 +0530

    feat: passing CubeSat ownership in & out

---
+++ ch4/src/bin/ch4-check-stats3.rs
@@ -0,0 +1,31 @@
+#[derive(Debug)]  // easy way to work this program is implement Copy (Clone, Copy) trait
+struct CubeSat {
+    id: u64,
+}
+
+#[derive(Debug)]
+enum StatusMessage {
+    Ok,
+}
+
+fn check_status(sat_id: CubeSat) -> CubeSat {
+    println!("{:?}: {:?}", sat_id, StatusMessage::Ok);
+    sat_id
+}
+
+fn main() {
+    let sat_a = CubeSat { id: 0 };
+    let sat_b = CubeSat { id: 1 };
+    let sat_c = CubeSat { id: 2 };
+
+    let sat_a = check_status(sat_a);
+    let sat_b = check_status(sat_b);
+    let sat_c = check_status(sat_c);
+    // println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
+
+    // waiting...
+    let a_status = check_status(sat_a);
+    let b_status = check_status(sat_b);
+    let c_status = check_status(sat_c);
+    // println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
+}

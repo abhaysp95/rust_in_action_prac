8ef123d283c574d37732a1c7564aebf9721e3499
coolabhays <abhaysp9955@gmail.com>
2022-11-21 11:21:19 +0530

    feat: using Rc<RefCell<T>> to mutate an immutable object

---
+++ ch4/src/bin/ch4-rc-refcell-groundstation.rs
@@ -0,0 +1,31 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+
+#[derive(Debug)]
+struct GroundStation {
+    radio_freq: f64,
+}
+
+fn main() {
+    let base: Rc<RefCell<GroundStation>> = Rc::new(RefCell::new(
+            GroundStation {
+                radio_freq: 87.65
+            }
+        ));
+
+    println!("base: {:?}", base);
+
+    {
+        let mut base2 = base.borrow_mut();
+        base2.radio_freq -= 12.34;
+        println!("base2: {:?}", base2);
+    }
+
+    println!("base: {:?}", base);
+
+    let mut base3 = base.borrow_mut();
+    base3.radio_freq += 43.21;
+
+    println!("base: {:?}", base);
+    println!("base3: {:?}", base3);
+}
